efdfa96dcd4aa9a6a7f2a941f746c56061a93861
Vladimir <vladimirleonov789@gmail.com>
2023-05-01 23:22:54 +0300

    feat: add prevent defaultfor textarea

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 import createContainerWithMainElements from './js/createContainerWithMainElements.js';
 
 createContainerWithMainElements();
+
+const textarea = document.querySelector(".textarea");
+const keys = document.querySelectorAll(".key");
+
+//prevent default textarea
+textarea.addEventListener("keydown", (e) => {
+  e.preventDefault();
+});
