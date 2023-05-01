bf3429b4c9804c239f36c1dd532720d1d84ce3c8
Vladimir <vladimirleonov789@gmail.com>
2023-05-01 23:24:08 +0300

    feat: add document keyup event

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const keys = document.querySelectorAll(".key");
 textarea.addEventListener("keydown", (e) => {
   e.preventDefault();
 });
+
+
+//remove class active on keyup
+document.addEventListener("keyup", (e) => {
+  console.log(e);
+  //Shift
+  if (e.key === "Shift") {
+    console.log('shift up');
+    e.preventDefault();
+    toggleShift(keys, "keyup");
+  }
+  keys.forEach((key) => {
+    if (key.className.includes("active") && !key.className.includes("CapsLock")) {
+      key.classList.remove("active");
+    }
+  });
+});
